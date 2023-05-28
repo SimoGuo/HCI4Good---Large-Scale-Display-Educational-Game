aa0ab67fb6161ec149b9fffbc95a174f329bd1dc
Aryah <ar341476@dal.ca>
2023-05-28 16:08:50 -0300

    multiple finger spawning working

--- Assets/Scripts/Spawner/Spawner.cs
+++ Assets/Scripts/Spawner/Spawner.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,54 +8,56 @@ public class Spawner : MonoBehaviour {
     private float width;
     private float height;
     private Vector2 screenPos;
+
+    private Dictionary<int, Transform> fingerToCharacter = new Dictionary<int, Transform>();
     private Transform currentCharacter;
+    private List<int> fingerIDs;
     // private Touch touch;
     private Ray camRay;
     [SerializeField] private Transform character;
     private void Awake() {
         width = Screen.width / 2f;
         height = Screen.height / 2f;
+        fingerIDs = new List<int>();
+    }
+
+    void MoveCharacter(Touch touch, Transform currentCharacter) {
+        // if (touch.phase == TouchPhase.Began) {
+        //     
+        // }
+        if (touch.phase == TouchPhase.Moved) {
+            currentCharacter.GetComponent<Controller>().Move(Camera.main.ScreenPointToRay(new Vector3(touch.position.x, touch.position.y, 0)).GetPoint(30));
+        }
+    }
+
+    private void Update() {
+
+        if (Input.touchCount > 0) {
+            foreach (Touch touch in Input.touches) {
+                if (touch.phase == TouchPhase.Began) {
+                    fingerToCharacter.TryAdd(touch.fingerId, Instantiate(character, Camera.main.ScreenPointToRay(new Vector3(touch.position.x, touch.position.y, 0)).GetPoint(30), Random.rotation));
+                }
+            }
+        }
+        
+        // foreach (KeyValuePair<int, Transform> i in fingerToCharacter) {
+        //     if (Input.touchCount == 0) break;
+        //     Touch touch = Input.touches[0];
+        //     for (int j = 0; j < Input.touchCount; j++) {
+        //         if (Input.touches[j].fingerId == i.Key) {
+        //             touch = Input.touches[j];
+        //             break;
+        //         }
+        //     }
+        //     MoveCharacter(touch, i.Value);
+        // }
     }
 
     void OnGUI() {
-        GUI.skin.label.fontSize = (int)(Screen.width / 25.0f);
-    
-        GUI.Label(new Rect(20, 20, width, height * 0.25f), Input.touchCount.ToString());
-    }
+        GUI.skin.label.fontSize = (int)(Screen.width / 100.0f);
+        string fingers = "";
 
-    
-    private void Update() {
-        if (Input.touchCount > 0) {
-            // touch = Input.GetTouch(0);
-            foreach (Touch touch in Input.touches) {
-                if (touch.phase == TouchPhase.Began) {
-                    camRay = Camera.main.ScreenPointToRay(new Vector3(touch.position.x, touch.position.y, 0));
-                    if (Physics.Raycast(camRay, out RaycastHit hit)) {
-                        Debug.Log("found char");
-                        if (hit.collider.CompareTag("Player")) {
-                            currentCharacter = hit.transform.gameObject.GetComponentInParent<Controller>().transform;
-                        }
-                        else {
-                            // if hit ground
-                            currentCharacter = Instantiate(character, hit.point + (Vector3.up * 5), Random.rotation);
-                        }
-                        
-                    }
-                    else {
-                        Debug.Log("creating char");
-                        currentCharacter = Instantiate(character, camRay.GetPoint(30), Random.rotation);
-                    }
-                }
-
-                if (touch.phase == TouchPhase.Moved) {
-                    camRay = Camera.main.ScreenPointToRay(new Vector3(touch.position.x, touch.position.y, 0));
-                    // touch = Input.GetTouch(0);
-                    if (currentCharacter != null) {
-                        currentCharacter.GetComponent<Controller>().Move(camRay.GetPoint(30));
-                    }
-                }
-            }
-            
-        }
+        
+        GUI.Label(new Rect(20, 20, width, height * 0.25f), fingers);
     }
 }
