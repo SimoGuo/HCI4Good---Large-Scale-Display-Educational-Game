a40e20fc5022546e896e395f51542c263a767350
Aryah <ar341476@dal.ca>
2023-05-28 21:59:13 -0300

    minor fixes

--- Assets/Scripts/Spawner/Spawner.cs
+++ Assets/Scripts/Spawner/Spawner.cs
@@ -1,57 +1,40 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class Spawner : MonoBehaviour {
     private float width;
     private float height;
-    private Vector2 screenPos;
-    private Touch[] touches;
-    private Dictionary<int, Transform> fingerToCharacter = new Dictionary<int, Transform>();
+    private Dictionary<Touch, Transform> fingerToCharacter = new Dictionary<Touch, Transform>();
     private Transform currentCharacter;
-    private List<int> fingerIDs;
-    // private Touch touch;
     private Ray camRay;
+    
     [SerializeField] private Transform character;
     private void Awake() {
         width = Screen.width / 2f;
         height = Screen.height / 2f;
-        fingerIDs = new List<int>();
-        touches = Input.touches;
     }
-
+    
     private void Update() {
-        touches = Input.touches;
-    }
-
-    private void FixedUpdate() {
-        
-        if (touches.Length == 0) {
-            fingerToCharacter.Clear();
-        }
-        
-        foreach (Touch touch in touches) {
+        foreach (Touch touch in Input.touches) {
             if (touch.phase == TouchPhase.Began) {
                 camRay = Camera.main.ScreenPointToRay(new Vector3(touch.position.x, touch.position.y, 0));
-
-                if (Physics.Raycast(camRay, out RaycastHit hit) && hit.collider.CompareTag("Player")) {
-                    fingerToCharacter.TryAdd(touch.fingerId, hit.transform.GetComponentInParent<Controller>().transform);
-                    continue;
-                }
-
-                if (!fingerToCharacter.ContainsKey(touch.fingerId)) {
-                    fingerToCharacter.Add(touch.fingerId, Instantiate(character, camRay.GetPoint((30)), Random.rotation));
+                
+                if (!fingerToCharacter.ContainsKey(touch)) {
+                    fingerToCharacter.Add(touch, Instantiate(character, camRay.GetPoint((30)), Random.rotation));
                 }
             }
+
         }
         
         
-        foreach (KeyValuePair<int, Transform> i in fingerToCharacter) {
-            foreach (Touch touch in touches) {
-                if (touch.fingerId == i.Key) {
-                    if (touch.phase == TouchPhase.Moved) {
+        foreach (KeyValuePair<Touch, Transform> i in fingerToCharacter) {
+            foreach (Touch touch in Input.touches) {
+                if (touch.fingerId == i.Key.fingerId) {
+                    if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary) {
                         camRay = Camera.main.ScreenPointToRay(new Vector3(touch.position.x, touch.position.y, 0));
                         i.Value.GetComponent<Controller>().Move(camRay.GetPoint(30));
                     }
