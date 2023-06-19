0cb23815c224d40923e80a1c0495c8aea3b01b53
Aryah <ar341476@dal.ca>
2023-06-19 11:14:41 -0300

    visually shows formation of maze

--- Assets/Scripts/Maze/Generator.cs
+++ Assets/Scripts/Maze/Generator.cs
@@ -13,6 +13,7 @@ public static class Generator {
         Right     = 1 << 3, // 00001000
         DownLeft  = 1 << 4, // 00010000
         DownRight = 1 << 5, // 00100000
+        Current   = 1 << 6,
         Visited   = 1 << 7  // 10000000
     }
 
@@ -40,20 +41,20 @@ public static class Generator {
         }
     }
 
-    private static NodeState[,] RecursiveBacktracker(NodeState[,] maze, int width, int height) {
+    private static IEnumerable<NodeState[,]> RecursiveBacktracker(NodeState[,] maze, int width, int height) {
 
         Random rng = new Random();
         Stack<Position> positions = new Stack<Position>();
         
         // random start
-        Position pos = new Position { x = rng.Next(0, width), y = rng.Next(0, height) };
-        // Position pos = new Position { x = 0, y = 0 };
+        // Position pos = new Position { x = rng.Next(0, width), y = rng.Next(0, height) };
+        Position pos = new Position { x = 0, y = 0 };
         maze[pos.x, pos.y] |= NodeState.Visited;
         positions.Push(pos);
 
         while (positions.Count > 0) {
             Position current = positions.Pop();
-
+            maze[current.x, current.y] |= NodeState.Current;
             List<Neighbour> neighbours = GetUnvisitedNeighbours(current, maze, width, height);
 
             if (neighbours.Count > 0) {
@@ -68,12 +69,11 @@ public static class Generator {
                 maze[randNeighbour.Position.x, randNeighbour.Position.y] |= NodeState.Visited;
                 
                 positions.Push(randNeighbour.Position);
+                yield return maze;
+                maze[current.x, current.y] &= ~NodeState.Current;
             }
         }
-        
-        
-        
-        return maze;
+        yield return maze;
     }
 
     private static List<Neighbour> GetUnvisitedNeighbours(Position p, NodeState[,] maze, int width, int height) {
@@ -167,7 +167,7 @@ public static class Generator {
     }
     
     
-    public static NodeState[,] Generate(int width, int height) {
+    public static IEnumerable<NodeState[,]> Generate(int width, int height) {
         NodeState[,] maze = new NodeState[width, height];
         NodeState start = NodeState.DownLeft | NodeState.Left | NodeState.Right | NodeState.UpLeft | NodeState.DownRight | NodeState.UpRight;
 
@@ -177,8 +177,9 @@ public static class Generator {
             }
         }
 
-        return RecursiveBacktracker(maze, width, height);
-        
+        foreach (NodeState[,] mazeStep in RecursiveBacktracker(maze, width, height)) {
+            yield return mazeStep;
+        }
     }
     
 }
--- Assets/Scripts/Maze/Renderer.cs
+++ Assets/Scripts/Maze/Renderer.cs
@@ -11,80 +11,95 @@ public class Renderer : MonoBehaviour {
     private float vertical;
     [SerializeField] private Transform wall;
     [SerializeField] private Transform center;
+
+    private Transform current;
     
-    void Render(Generator.NodeState[,] maze) {
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                Generator.NodeState node = maze[i, j];
-
-                string name = i.ToString() + j.ToString();
-                horizontal = wall.localScale.x * Mathf.Cos(30 * Mathf.Deg2Rad);
-                vertical = wall.localScale.x + (wall.localScale.x * Mathf.Sin(30 * Mathf.Deg2Rad));
-                
-                Vector3 pos = new Vector3((i - width / 2) * horizontal * 2, 0, (j - height / 2) * vertical); // center of node
-                
-                if (j % 2 == 0) {
-                    pos -= new Vector3(horizontal, 0, 0);
+    IEnumerator Render() {
+        
+        foreach (Generator.NodeState[,] maze in Generator.Generate(width, height)) {
+            foreach (Transform t in transform) {
+                if (t.name != "current") {
+                    Destroy(t.gameObject);
                 }
-                
-                Instantiate(center, transform);
-                center.position = pos;
-                center.name = name;
-
-                if (node.HasFlag(Generator.NodeState.UpLeft)) {
-                    Transform upLeft = Instantiate(wall, transform);
-                    upLeft.eulerAngles = new Vector3(0, -30, 0);
-                    upLeft.position = pos + new Vector3(-horizontal / 2, 0, vertical / 2);
-                    upLeft.name = "upLeft" + name;
-                    
-                }
-                // //
-                if (node.HasFlag(Generator.NodeState.Left)) {
-                    Transform left = Instantiate(wall, transform);
-                    left.eulerAngles = new Vector3(0, 90, 0);
-                    left.position = pos + new Vector3(-horizontal, 0, 0);
-                    left.name = "left" + name;
-                }
-                // //
-                if (node.HasFlag(Generator.NodeState.DownLeft)) {
-                    Transform downLeft = Instantiate(wall, transform);
-                    downLeft.eulerAngles = new Vector3(0, 30, 0);
-                    downLeft.position = pos + new Vector3(-horizontal / 2, 0, -vertical / 2);
-                    downLeft.name = "downLeft" + name;
-                }
-
-
-                // if (i == width - 1) {
-                //     if (node.HasFlag(Generator.NodeState.Right)) {
-                //         Transform right = Instantiate(wall, transform);
-                //         right.eulerAngles = new Vector3(0, 90, 0);
-                //         right.position = pos + new Vector3(wall.localScale.x / 2, 0, 0);
-                //     }
-                // }
-                //
-                // if (j == 0) {
-                //     if (node.HasFlag(Generator.NodeState.DownLeft)) {
-                //         Transform down = Instantiate(wall, transform);
-                //         down.position = pos + new Vector3(0, 0, -wall.localScale.x / 2);
-                //     }
-                // }
             }
-        }    
+            for (int i = 0; i < width; i++) {
+                for (int j = 0; j < height; j++) {
+                    Generator.NodeState node = maze[i, j];
+
+                    string name = i.ToString() + j.ToString();
+                    horizontal = wall.localScale.x * Mathf.Cos(30 * Mathf.Deg2Rad);
+                    vertical = wall.localScale.x + (wall.localScale.x * Mathf.Sin(30 * Mathf.Deg2Rad));
+
+                    Vector3 pos = new Vector3((i - width / 2) * horizontal * 2, 0,
+                        (j - height / 2) * vertical); // center of node
+
+                    if (j % 2 == 0) {
+                        pos -= new Vector3(horizontal, 0, 0);
+                    }
+
+                    if (node.HasFlag(Generator.NodeState.Current)) {
+                        current.position = pos;
+                    }
+                    
+
+                    if (node.HasFlag(Generator.NodeState.UpLeft)) {
+                        Transform upLeft = Instantiate(wall, transform);
+                        upLeft.eulerAngles = new Vector3(0, -30, 0);
+                        upLeft.position = pos + new Vector3(-horizontal / 2, 0, vertical / 2);
+                        upLeft.name = "upLeft" + name;
+
+                    }
+
+                    // //
+                    if (node.HasFlag(Generator.NodeState.Left)) {
+                        Transform left = Instantiate(wall, transform);
+                        left.eulerAngles = new Vector3(0, 90, 0);
+                        left.position = pos + new Vector3(-horizontal, 0, 0);
+                        left.name = "left" + name;
+                    }
+
+                    // //
+                    if (node.HasFlag(Generator.NodeState.DownLeft)) {
+                        Transform downLeft = Instantiate(wall, transform);
+                        downLeft.eulerAngles = new Vector3(0, 30, 0);
+                        downLeft.position = pos + new Vector3(-horizontal / 2, 0, -vertical / 2);
+                        downLeft.name = "downLeft" + name;
+                    }
+
+
+                    // if (i == width - 1) {
+                    //     if (node.HasFlag(Generator.NodeState.Right)) {
+                    //         Transform right = Instantiate(wall, transform);
+                    //         right.eulerAngles = new Vector3(0, 90, 0);
+                    //         right.position = pos + new Vector3(wall.localScale.x / 2, 0, 0);
+                    //     }
+                    // }
+                    //
+                    // if (j == 0) {
+                    //     if (node.HasFlag(Generator.NodeState.DownLeft)) {
+                    //         Transform down = Instantiate(wall, transform);
+                    //         down.position = pos + new Vector3(0, 0, -wall.localScale.x / 2);
+                    //     }
+                    // }
+                }
+            }
+
+            yield return new WaitForSeconds(.05f);
+        }
     }
     
     private void Start() {
-        
-        
+        current = Instantiate(center);
+        current.name = "current";
     }
 
     private void Update() {
         if (Input.GetKeyDown(KeyCode.Space)) {
-            
+            StopCoroutine(Render());
             foreach (Transform t in transform) {
                 Destroy(t.gameObject);
             }
-
-            Render(Generator.Generate(width, height));
+            StartCoroutine(Render());
         }
     }
 }
