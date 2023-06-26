199cc8ec1850513edc2576ffa281778a0f1b97ec
Nguyen Phuc Le <ng576796@dal.ca>
2023-06-26 06:45:53 -0300

    Fixed bugs in Enemy script, Successfully tested Enemy script

---
+++ Assets/Scripts/Enemies/EnemyMeleeUnit.cs
@@ -0,0 +1,125 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMeleeUnit: MonoBehaviour
+{
+    public NavMeshAgent agent;
+    
+    //Player position
+    public Transform player;
+    public LayerMask groundLayer, playerLayer;
+
+    //Patrolling
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    public Vector3 distanceToWalkPoint;
+    public float distance;
+
+    private void Awake()
+    {
+        //Get the player position through name (Script is for 1 player only, will change to more later)
+        player = GameObject.Find("PlayerCharacter").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //Check for player in sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, playerLayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, playerLayer);
+
+        if (!playerInSightRange && !playerInAttackRange)
+        {
+            Patrolling();
+        }
+        if (playerInSightRange && !playerInAttackRange)
+        {
+            ChasePlayer();
+        }
+        if (playerInSightRange && playerInAttackRange)
+        {
+            AttackPlayer();
+        }
+    }
+
+    private void Patrolling()
+    {
+        if (!walkPointSet) {
+            SearchWalkPoint();
+        }
+
+        if (walkPointSet)
+        {
+            agent.SetDestination(walkPoint);
+        }
+
+        distanceToWalkPoint = transform.position - walkPoint;
+        distance = distanceToWalkPoint.magnitude;
+
+        //Walkpoint reached (Will not work if NavAgent stopping distance > 1)
+        if (distanceToWalkPoint.magnitude < 1) {
+        walkPointSet = false;
+        }
+    }
+
+    private void SearchWalkPoint()
+    {
+        //Caculate random point in range
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3 (transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, groundLayer))
+        {
+            walkPointSet = true;
+        }
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        //Make the enemy stop before attacking
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        if (!alreadyAttacked)
+        {
+            //Attack code here
+
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
+}
