87469d2b69884743176b5036e5f84eb0ce1bdb86
Nguyen Phuc Le <ng576796@dal.ca>
2023-06-26 09:15:08 -0300

    Added new scene and script used for testing and generating NavMesh in runtime

--- Assets/Scripts/Maze/NavMeshBaker.cs
+++ Assets/Scripts/Maze/NavMeshBaker.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.AI.Navigation;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NavMeshBaker : MonoBehaviour
 {
-    Nav
+    public NavMeshSurface surface;
     // Start is called before the first frame update
     void Start()
     {
         //UPDATE NAVMESH
+        surface.BuildNavMesh();
     }
 
     // Update is called once per frame
