49e5b72d70e83fb47f6f87fc1b775f696fb07762
Safat Sadnan Snehal <sf231351@dal.ca>
2023-07-13 18:59:24 -0300

    added comments

--- Assets/Scripts/PlayerMeleeCombat.cs
+++ Assets/Scripts/PlayerMeleeCombat.cs
@@ -6,22 +6,23 @@ public class PlayerMeleeCombat : MonoBehaviour
 {
     // Start is called before the first frame update
    //public Animator animator;
-   public List<Collider> detectedColliders = new List<Collider>();
+   public List<Collider> detectedColliders = new List<Collider>(); // lit of detected enemies
    Collider col;
    private void Awake(){
-    col = GetComponent<Collider>();
+    col = GetComponent<Collider>(); // use Collider Object
    }
 
-    private void OnTriggerEnter(Collider collision)
+    private void OnTriggerEnter(Collider collision) // When the enemy enters the zone it triggers
     {
-        detectedColliders.Add(collision);
+        detectedColliders.Add(collision); // Adds the number of enemies detected
         
     }
 
     private void OnTriggerExit(Collider collision)
     {
-        detectedColliders.Remove(collision);
+        detectedColliders.Remove(collision); // Removes the enemy from the list if the range becomes far from the player
     }
 
-   
+    
+      
 }
