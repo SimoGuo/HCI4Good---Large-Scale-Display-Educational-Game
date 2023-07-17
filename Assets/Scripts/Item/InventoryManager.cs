ef0082a0f8e11dca1fe715a916ce209c0ac3daf4
Nguyen Phuc Le <ng576796@dal.ca>
2023-07-17 03:38:01 -0300

    Added Inventory UI in new Scene

---
+++ Assets/Scripts/Item/InventoryManager.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager instance;
+    public List<Item> items = new List<Item>();
+
+    public Transform itemContent;
+    public GameObject inventoryItem;
+
+    public UnityEngine.UI.Toggle RemoveItemToggle;
+    public InventoryItemController[] inventoryItems;
+
+    private void Awake()
+    {
+        instance = this; 
+    }
+
+    public void Add(Item item)
+    {
+        items.Add(item);
+    }
+
+    public void Remove(Item item)
+    {
+        items.Remove(item);
+    }
+
+    public void ListItems()
+    {
+        foreach (var item in items)
+        {
+            GameObject obj = Instantiate(inventoryItem, itemContent);
+            var removeButton = obj.GetComponent<UnityEngine.UI.Button>();
+            var itemName = obj.transform.Find("ItemName").GetComponent<TextMeshProUGUI>();
+            var itemIcon = obj.transform.Find("ItemIcon").GetComponent<UnityEngine.UI.Image>();
+           
+
+            itemName.text = item.itemName;
+            itemIcon.sprite = item.sprite;
+
+            if (RemoveItemToggle.isOn)
+            {
+                removeButton.gameObject.SetActive(true);
+            }
+        }
+        SetInventoryItems();
+    }
+
+    public void EnableItemRemove()
+    {
+        if (RemoveItemToggle.isOn) 
+        {
+            foreach(Transform item in itemContent)
+            {
+                item.Find("RemoveButton").gameObject.SetActive(true);
+            }
+        }
+        else
+        {
+            foreach(Transform item in itemContent)
+            {
+                item.Find("RemoveButton").gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void SetInventoryItems()
+    {
+        inventoryItems = itemContent.GetComponentsInChildren<InventoryItemController>();
+
+        for(int i = 0; i < items.Count; i++)
+        {
+            inventoryItems[i].AddItem(items[i]);
+        }
+    }
+}
