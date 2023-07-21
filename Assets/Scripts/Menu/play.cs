9d9f964b8690c1f600d212c14274504fb217e5a5
Sahara <sh320914@dal.ca>
2023-07-21 11:21:09 -0300

    End and all logic update

--- Assets/Scripts/Menu/play.cs
+++ Assets/Scripts/Menu/play.cs
@@ -5,8 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class play : MonoBehaviour
 {
+    private float startTime;
+
     public void loadCharacters()
     {
+        startTime = Time.time;
+        PlayerPrefs.SetFloat("startTime", startTime);
         SceneManager.LoadScene("Characters");
     }
 
