2d71b2a403863f5a6aed815c1107c98996be10cf
bvarsani <br667430@dal.ca>
2023-07-22 10:08:14 -0300

    fixed collision bug and made it little bit smooth

--- Assets/Scripts/PlayerCharacter/Player.cs
+++ Assets/Scripts/PlayerCharacter/Player.cs
@@ -1,4 +1,4 @@
-using System;
+/*using System;
 using PlayerCharacter.Interfaces;
 using PlayerCharacter.PlayerStateMachine.ScriptableObjectsBase;
 using PlayerCharacter.PlayerStateMachine.States;
@@ -19,6 +19,7 @@ namespace PlayerCharacter {
         [SerializeField] public SphereCollider attackZone;
         public bool InAttackRange { get; private set; }
         public EnemyMeleeUnit TargetedEnemy { get; private set; }
+        public character enemy;
 
         [SerializeField] private PlayerIdleSO playerIdleBase;
         [SerializeField] private PlayerMoveSO playerMoveBase;
@@ -69,6 +70,7 @@ namespace PlayerCharacter {
             if (_stateMachine.CurrentPlayerState == null) Debug.Log("currentState null");
             _stateMachine.CurrentPlayerState.FrameUpdate();
             Debug.Log(rb.velocity.magnitude);
+            DealDamageToCharacter(20f);
             _anim.SetFloat("Speed", rb.velocity.magnitude);
             _anim.SetBool("Attack", InAttackRange);
 
@@ -92,7 +94,20 @@ namespace PlayerCharacter {
             }
         }
 
-        public void Move() {
+        private void DealDamageToCharacter(float damageAmount)
+        {
+            // Find the character component attached to the GameObject
+            character characterComponent = GetComponent<character>();
+
+            // Check if the character component is found
+            if (characterComponent != null)
+            {
+                // Call the TakeDamage method to deal damage
+                characterComponent.TakeDamage(damageAmount);
+            }
+        }
+
+            private void Move() {
             if (NeedsTarget) {
                 rb.velocity = Vector3.zero;
                 return;
@@ -114,6 +129,8 @@ namespace PlayerCharacter {
             if (other.CompareTag("Enemy")) {
                 InAttackRange = true;
                 TargetedEnemy = other.GetComponent<EnemyMeleeUnit>();
+                enemy = other.GetComponent<character>();
+                enemy.TakeDamage(20f);
             }
         }
 
@@ -149,5 +166,179 @@ namespace PlayerCharacter {
         {
             PlayerBuffInstance = _playerBuffInstance;
         }
+    }
+}
+*/
+
+
+using System;
+using System.Diagnostics;
+using PlayerCharacter.Interfaces;
+using PlayerCharacter.States;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Serialization;
+using System.Collections;
+
+namespace PlayerCharacter
+{
+    public class Player : MonoBehaviour, IDamageable
+    {
+        public bool NeedsTarget { set; get; } = true;
+        public Vector3 Target { set; get; }
+        public Rigidbody rb { get; set; }
+        public Animator Anim { get; private set; }
+        private PlayerStateMachine _stateMachine;
+        private PlayerAttackState _attackState;
+        private PlayerMoveState _moveState;
+        private PlayerIdleState _idleState;
+        [SerializeField] public SphereCollider attackZone;
+        public bool InAttackRange { get; private set; }
+        public EnemyMeleeUnit TargetedEnemy { get; private set; }
+        public character enemy;
+
+        [SerializeField] private PlayerIdleSO playerIdleBase;
+        [SerializeField] private PlayerMoveSO playerMoveBase;
+        [SerializeField] private PlayerAttackSO playerAttackBase;
+
+        public PlayerIdleSO PlayerIdleInstance { get; private set; }
+        public PlayerMoveSO PlayerMoveInstance { get; private set; }
+        public PlayerAttackSO PlayerAttackInstance { get; private set; }
+
+        private bool isAttacking = false;
+        private bool canAttack = true;
+        [SerializeField] private float attackCooldownDuration = 1.5f;
+
+        private void Awake()
+        {
+            PlayerIdleInstance = Instantiate(playerIdleBase);
+            PlayerMoveInstance = Instantiate(playerMoveBase);
+            PlayerAttackInstance = Instantiate(playerAttackBase);
+
+            _stateMachine = new PlayerStateMachine();
+
+            _idleState = new PlayerIdleState(this, _stateMachine);
+            _moveState = new PlayerMoveState(this, _stateMachine);
+            _attackState = new PlayerAttackState(this, _stateMachine);
+        }
+
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+            rb.freezeRotation = true;
+            Anim = GetComponent<Animator>();
+
+            PlayerIdleInstance.Initialize(gameObject, this);
+            PlayerMoveInstance.Initialize(gameObject, this);
+            PlayerAttackInstance.Initialize(gameObject, this);
+
+            _stateMachine.Init(_idleState);
+        }
+
+        private void FixedUpdate()
+        {
+            Move();
+            _stateMachine.CurrentPlayerState.PhysicsUpdate();
+        }
+
+        private void Update()
+        {
+            _stateMachine.CurrentPlayerState.FrameUpdate();
+            Anim.SetFloat("Speed", rb.velocity.magnitude);
+            Anim.SetBool("Attack", InAttackRange);
+
+            // Only attack if not already attacking and not on cooldown
+            if (!isAttacking && canAttack && InAttackRange)
+            {
+                StartCoroutine(AttackRoutine());
+            }
+
+            if (rb.velocity.magnitude <= 0.01f)
+            {
+                UnityEngine.Debug.Log("idling");
+                _stateMachine.ChangeState(_idleState);
+            }
+            else
+            {
+                UnityEngine.Debug.Log("moving");
+                _stateMachine.ChangeState(_moveState);
+            }
+        }
+
+        private IEnumerator AttackRoutine()
+        {
+            isAttacking = true;
+            Anim.SetTrigger("Attack");
+            yield return new WaitForSeconds(PlayerAttackInstance.AttackDuration);
+            DealDamageToCharacter(5f);
+            canAttack = false;
+            yield return new WaitForSeconds(attackCooldownDuration);
+            canAttack = true;
+            isAttacking = false;
+        }
+
+        private void DealDamageToCharacter(float damageAmount)
+        {
+            character characterComponent = GetComponent<character>();
+            if (characterComponent != null)
+            {
+                characterComponent.TakeDamage(damageAmount);
+            }
+        }
+
+        private void Move()
+        {
+            if (NeedsTarget)
+            {
+                rb.velocity = Vector3.zero;
+                return;
+            };
+
+            transform.LookAt(new Vector3(Target.x, transform.position.y, Target.z));
+            if (Vector3.Distance(transform.position, Target) <= PlayerMoveInstance.StoppingDistance)
+            {
+                rb.velocity = Vector3.zero;
+            }
+            else
+            {
+                if (rb.velocity.magnitude <= PlayerMoveInstance.MaxSpeed)
+                {
+                    rb.AddForce(transform.forward * PlayerMoveInstance.Speed, ForceMode.Acceleration);
+                }
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Enemy"))
+            {
+                InAttackRange = true;
+                TargetedEnemy = other.GetComponent<EnemyMeleeUnit>();
+                enemy = other.GetComponent<character>();
+                enemy.TakeDamage(20f);
+            }
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Enemy"))
+            {
+                InAttackRange = false;
+                TargetedEnemy = null;
+            }
+        }
+
+        public void Damage(float amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Kill()
+        {
+            throw new NotImplementedException();
+        }
+
+        public float maxHealth { get; set; }
+        public float currentHealth { get; set; }
     }
 }
