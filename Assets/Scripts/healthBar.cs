31ae2d736b3d7678f2942787ca8a809276b77115
Le <ng576796@dal.ca>
2023-07-26 09:38:27 -0300

    HLSDEG-96: Created new status bar with added effects

--- Assets/Scripts/healthBar.cs
+++ Assets/Scripts/healthBar.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,31 +6,84 @@ using UnityEngine.UI;
 
 public class healthBar : MonoBehaviour
 {
-    public Slider slider;
+    public Slider frontHealthBarSlider;
+    public Slider backHealthBarSlider;
+    private float currentHealth;
+    [SerializeField] private float maxHealth;
+    private float lerpTimer;
+    [SerializeField] private float chipSpeed;
 
-    public void SetMaxHealth(int maxHealth)
+    private ColorBlock color = new ColorBlock();
+
+    private void Start()
     {
-        slider.maxValue = maxHealth;
-        slider.value = maxHealth;
+        currentHealth = maxHealth;
+        SetMaxHealth(maxHealth);
     }
 
-    public void Sethealth(int health)
+    public void Update()
     {
-        slider.value = health;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            TakeDamage(10);
+            UpdateUI();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Heal(5);
+            UpdateUI();
+        }
     }
 
-    public int getHealth()
+    public void UpdateUI()
     {
-        return (int)slider.value;
+        Debug.Log(currentHealth);
+
+        //Every value is coded in fraction from 0 to 1 because maxHealth is not always 100
+        float fillFrontHealthBar = frontHealthBarSlider.value;
+        float fillBackHealthBar = backHealthBarSlider.value;
+        float healthFraction = currentHealth / maxHealth;
+        
+        if (fillBackHealthBar / backHealthBarSlider.maxValue > healthFraction)
+        {
+            frontHealthBarSlider.value = healthFraction*maxHealth;
+            color.normalColor = Color.blue;
+            backHealthBarSlider.colors = color;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpeed;
+            percentComplete = percentComplete * percentComplete;
+            backHealthBarSlider.value = Mathf.Lerp(fillBackHealthBar* backHealthBarSlider.maxValue, healthFraction*maxHealth, percentComplete);
+        }
+        if(fillBackHealthBar /  backHealthBarSlider.maxValue < healthFraction)
+        {
+            color.normalColor = Color.green;
+            backHealthBarSlider.colors = color;
+            backHealthBarSlider.value = healthFraction*maxHealth;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpeed;
+            percentComplete = percentComplete * percentComplete;
+            frontHealthBarSlider.value = Mathf.Lerp(fillFrontHealthBar*frontHealthBarSlider.maxValue, healthFraction*maxHealth, percentComplete);
+        }
+    }
+    public void SetMaxHealth(float maxHealth)
+    {
+        frontHealthBarSlider.maxValue = maxHealth;
+        frontHealthBarSlider.value = maxHealth;
+        backHealthBarSlider.maxValue = maxHealth;
+        backHealthBarSlider.value = maxHealth;
     }
 
-    public void IncreaseHealth(int health)
+    public void TakeDamage(float damage)
     {
-        slider.value += health;
+        currentHealth -= damage;
+        lerpTimer = 0f;
     }
 
-    public void DecreaseHealth(int health)
+    public void Heal (float heal)
     {
-        slider.value -= health;
+        currentHealth += heal;
+        lerpTimer = 0f;
     }
 }
