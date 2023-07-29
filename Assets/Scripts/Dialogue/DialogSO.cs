366e98f98635a4e1a582911a4c56a12209a478be
Aryah <aryah@dal.ca>
2023-07-28 21:35:26 -0300

    fixed

--- Assets/Scripts/Dialogue/DialogSO.cs
+++ Assets/Scripts/Dialogue/DialogSO.cs
@@ -1,3 +1,4 @@
+/*
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,3 +9,4 @@ public class DialogSO : ScriptableObject
 {
     public List<string> dialogList;
 }
+*/
