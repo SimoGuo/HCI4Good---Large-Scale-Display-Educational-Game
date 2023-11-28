12577e1691b19199b5c2665eca8a8edb529b2cd3
Lawrence Rhodenizer <lw441490@dal.ca>
2023-11-28 18:28:23 -0400

    added block teleport and block pressure plate script, started on a theme for level 2, block pushing with teleport mechanics. First part of the level is complete besides characters being present and cube being in scene.

---
+++ Assets/Scripts/BlockScripts/BlockTeleport.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockTeleport : MonoBehaviour
+{
+
+    [SerializeField] GameObject cube;
+
+    [SerializeField] Vector3 TeleportLocation;
+    
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider col){
+        
+        if(col.CompareTag("Cube")){
+            cube.transform.position = TeleportLocation;
+        }
+        
+    }
+}
